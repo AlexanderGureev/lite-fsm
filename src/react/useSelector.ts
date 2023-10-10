44b617cb02cf97ca024ff022d3feb7376f93262c
Александр Гуреев <alexga@MacBook-Pro-Aleksandr.local>
2023-10-10 19:12:02 +0300

    add useSyncExternalStoreWithSelector

--- src/react/useSelector.ts
+++ src/react/useSelector.ts
@@ -3,6 +3,9 @@ import React from "react";
 import { FSMContext, FSMContextType } from "./FSMContext";
 import { MachineConfig, MachinesState } from "~/core/types";
 
+import useSyncExternalStoreExports from "use-sync-external-store/shim/with-selector";
+const { useSyncExternalStoreWithSelector } = useSyncExternalStoreExports;
+
 export const useSelector = <
   S extends {
     [key in string]: MachineConfig<any, any, any, any>;
@@ -12,23 +15,37 @@ export const useSelector = <
   selector: (state: MachinesState<S>) => R,
   equalityFn?: (oldValue: R, newValue: R) => boolean,
 ) => {
-  const m = React.useContext<FSMContextType>(FSMContext);
-  const [state, set] = React.useState<R>(selector(m.getState()));
-
-  React.useEffect(() => {
-    return m.onTransition((prevState, currentState) => {
-      const prev = selector(prevState);
-      const next = selector(currentState);
-
-      if (equalityFn) {
-        const isEqual = equalityFn(prev, next);
-        if (!isEqual) set(next);
-        return;
-      }
-
-      if (prev !== next) set(next);
-    });
-  }, [m, selector, equalityFn]);
-
-  return state;
+  const api = React.useContext<FSMContextType>(FSMContext);
+  const slice = useSyncExternalStoreWithSelector(api.onTransition, api.getState, api.getState, selector, equalityFn);
+  return slice;
 };
+
+// export const useSelector = <
+//   S extends {
+//     [key in string]: MachineConfig<any, any, any, any>;
+//   },
+//   R,
+// >(
+//   selector: (state: MachinesState<S>) => R,
+//   equalityFn?: (oldValue: R, newValue: R) => boolean,
+// ) => {
+//   const m = React.useContext<FSMContextType>(FSMContext);
+//   const [state, set] = React.useState<R>(selector(m.getState()));
+
+//   React.useEffect(() => {
+//     return m.onTransition((prevState, currentState) => {
+//       const prev = selector(prevState);
+//       const next = selector(currentState);
+
+//       if (equalityFn) {
+//         const isEqual = equalityFn(prev, next);
+//         if (!isEqual) set(next);
+//         return;
+//       }
+
+//       if (prev !== next) set(next);
+//     });
+//   }, [m, selector, equalityFn]);
+
+//   return state;
+// };
