9272a17eb4a93f11a26e66913e597f31a199fcb2
Александр Гуреев <alexga@MacBook-Pro-Aleksandr.local>
2025-05-01 19:33:21 +0300

    feat: fix basePath

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,8 +1,15 @@
 import nextra from "nextra";
 
+// Определение, находимся ли мы в production
+const isProduction = process.env.NODE_ENV === "production";
+const basePath = isProduction ? "/lite-fsm" : "";
+
 // Настраиваем Nextra с ее конфигурацией
 const withNextra = nextra({
   // Дополнительные опции Nextra
+  search: {
+    codeblocks: true,
+  },
 });
 
 // Экспортируем конечную конфигурацию Next.js с Nextra
@@ -12,6 +19,7 @@ export default withNextra({
   images: {
     unoptimized: true,
   },
-  // Если проект размещен не в корне, нужно добавить basePath
-  basePath: process.env.NODE_ENV === "production" ? "/lite-fsm" : undefined,
+  // Если проект размещен не в корне, нужно добавить basePath и assetPrefix
+  basePath: basePath,
+  assetPrefix: basePath,
 });
